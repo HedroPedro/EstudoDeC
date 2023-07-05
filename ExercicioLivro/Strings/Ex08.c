a7dc157b70ff42c829a33785648b6f9fa0b5f6e6
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2023-07-05 09:29:19 -0300

    Testes

--- ExercicioLivro/Strings/Ex08.c
+++ ExercicioLivro/Strings/Ex08.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-
+//TODO: terminar esse
 int main(){
-    char string[20], string2[10];
-    int i, j, totalLetra;
-    int soma= 0;
+    char string[20], string2[10], substring[10];
+    int pos, j, totalLetra, existeSub, count = 0;
     fgets(string, 20, stdin);
     fgets(string2, 10, stdin);
     totalLetra = strlen(string2)-1;
-    printf("%i", totalLetra);
 
-    for(i = 0; i < strlen(string); i++){
-        for(j = totalLetra; j < -1; j--){
-
+    for(pos = 0; pos < strlen(string)-1; pos++){
+        while(count < totalLetra){
+            substring[count] = string[pos+count];
+            count++;
         }
-    }
-
-    if(soma != 0){
-        printf("A string %s esta contida na string %s", string2, string);
-    }else{
-        printf("A string %s nao esta contida na string %s", string2, string);
+        substring[count] = '\0';
+        printf("%s == %s\n", substring, string2);
+        count = 0;
+        strstr
     }
 }
