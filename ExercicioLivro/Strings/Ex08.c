168e1183b7f0b6f780f0a2624cc45e3d47e5ce49
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2023-07-05 09:31:38 -0300

    testes

--- ExercicioLivro/Strings/Ex08.c
+++ ExercicioLivro/Strings/Ex08.c
@@ -2,13 +2,13 @@
 #include <string.h>
 //TODO: terminar esse
 int main(){
-    char string[20], string2[10], substring[10];
+    char string[20], string2[20];
     int pos, j, totalLetra, existeSub, count = 0;
     fgets(string, 20, stdin);
     fgets(string2, 10, stdin);
     totalLetra = strlen(string2)-1;
 
-    for(pos = 0; pos < strlen(string)-1; pos++){
+    for(pos = 0; pos < strlen(string); pos++){
         while(count < totalLetra){
             substring[count] = string[pos+count];
             count++;
