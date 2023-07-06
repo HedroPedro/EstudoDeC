93057aa04e9d8b6632dc2f364a9cee0f0f401120
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2023-07-06 00:26:43 -0300

    feito exercicio 8 e 9

--- ExercicioLivro/Strings/Ex08.c
+++ ExercicioLivro/Strings/Ex08.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <string.h>
-//TODO: terminar esse
+
 int main(){
-    
-    int soma1, soma2 = 0;
+    char string[20], strOutra[20];
+    int somaDifs, estaContida = 0;
+    int i, j;
+    printf("Digite uma palavra: ");
+    fgets(string, 20, stdin);
+    printf("Digite outra palavra: ");
+    fgets(strOutra, 20, stdin);
+    for(i = 0; i < strlen(string)-1; i++){
+        for(j = 0; j < strlen(strOutra)-1; j++){
+                somaDifs +=  string[j+i] - strOutra[j];
+        }
+        printf("%i\n", somaDifs);
+        if(somaDifs == 0){
+            estaContida++;
+        }
+        somaDifs = 0;
+    }
+    if(estaContida > 0){
+        printf("A string %s esta dentro de %s", strOutra, string);
+    }else{
+        printf("A string %s nao esta dentro de %s", strOutra, string);
+    }
 }
---
+++ ExercicioLivro/Strings/Ex09.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+
+int main(){
+    char string[20], strOutra[20];
+    int somaDifs, estaContida = 0;
+    int i, j;
+    printf("Digite uma palavra: ");
+    fgets(string, 20, stdin);
+    printf("Digite outra palavra: ");
+    fgets(strOutra, 20, stdin);
+    for(i = 0; i < strlen(string)-1; i++){
+        for(j = 0; j < strlen(strOutra)-1; j++){
+                somaDifs +=  string[j+i] - strOutra[j];
+        }
+        printf("%i\n", somaDifs);
+        if(somaDifs == 0){
+            estaContida++;
+        }
+        somaDifs = 0;
+    }
+    if(estaContida > 0){
+        printf("A string %s esta dentro de %s, %i vezes", strOutra, string, estaContida);
+    }else{
+        printf("A string %s nao esta dentro de %s", strOutra, string);
+    }
+}
