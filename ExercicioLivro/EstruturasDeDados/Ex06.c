04af82124e8c4485be5b314509ce84c5baf6302b
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2023-07-12 09:18:37 -0300

    .

---
+++ ExercicioLivro/EstruturasDeDados/Ex06.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct{
+    char matricula[20];
+    int nota1, nota2, nota3;
+    float media;
+} aluno;
+
+int main(){
+    aluno alunos[3];
+    for(int i = 0; i < 3; i++){
+        
+    }
+}
