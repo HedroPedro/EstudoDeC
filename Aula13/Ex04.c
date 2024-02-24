603dd6d8486d1938e8ee3472493a4d4243b4900a
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2024-02-24 10:14:42 -0300

    a

--- Aula13/Ex04.c
+++ Aula13/Ex04.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXPATH 129
 
 int main(){
-    char filePath[MAXPATH], *content;
+    char filePath[MAXPATH], *content, tmpChar;
     FILE *currentFile;
+    printf("Digite o nome do primeiro arquivo: ");
+    setbuf(stdin, NULL);
 }
